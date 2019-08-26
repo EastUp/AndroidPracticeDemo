2a319e35b532b920130a88ebcddcdcb313efc379
east <eastrisewm@163.com>
2019-08-26 18:06:45 +0800

    初步测算navigation功能

--- navigation/src/main/java/com/east/navigation/MainActivity.kt
+++ navigation/src/main/java/com/east/navigation/MainActivity.kt
@@ -2,11 +2,18 @@ package com.east.navigation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        button.setOnClickListener {
+            it.findNavController().navigate(R.id.action_blankFragment3_to_blankFragment)
+        }
+
     }
 }
