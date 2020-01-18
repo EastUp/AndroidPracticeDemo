718c05f332411f9bce932e6b0eafc545f9298a48
east <eastrisewm@163.com>
2020-01-18 17:20:37 +0800

    面向对象六大原则-网络引擎切换

--- architect_zenghui/src/main/java/com/east/architect_zenghui/MainActivity.kt
+++ architect_zenghui/src/main/java/com/east/architect_zenghui/MainActivity.kt
@@ -1,12 +1,23 @@
 package com.east.architect_zenghui
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import com.east.architect_zenghui.change_network_engine.simple3.MainActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun onClick(v:View){
+        when(v){
+            //面向对象6大原则-网络引擎切换
+            change_netword_engine -> startActivity(Intent(this,MainActivity::class.java))
+        }
     }
 }
