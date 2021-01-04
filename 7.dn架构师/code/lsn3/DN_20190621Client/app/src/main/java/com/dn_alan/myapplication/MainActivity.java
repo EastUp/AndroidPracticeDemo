8d61c488be249c0adcf8552febff10e71eec9c12
东方 <eastrisewm@163.com>
2021-01-04 18:08:05 +0800

    ...

--- 7.dn架构师/code/lsn3/DN_20190621Client/app/src/main/java/com/dn_alan/myapplication/MainActivity.java
+++ 7.dn架构师/code/lsn3/DN_20190621Client/app/src/main/java/com/dn_alan/myapplication/MainActivity.java
@@ -4,11 +4,10 @@ import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.content.ServiceConnection;
+import android.os.Bundle;
 import android.os.IBinder;
 import android.os.RemoteException;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
