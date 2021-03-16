1ef40a86799be13efbf6b86d6655e89944772ecf
东方 <eastrisewm@163.com>
2021-03-16 11:56:53 +0800

    ...

---
+++ 7.dn架构师/code/lsn23/DN_ALAN_PLUGIN_23/taopiaopiao/src/main/java/com/dn_alan/taopiaopiao/BaseActivity.java
@@ -0,0 +1,82 @@
+package com.dn_alan.taopiaopiao;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import com.dn_alan.pluginstand.PayInterfaceActivity;
+
+public class BaseActivity extends Activity implements PayInterfaceActivity {
+    protected Activity that;
+
+    @Override
+    public void attach(Activity proxyActivity) {
+        this.that = proxyActivity;
+    }
+
+    @Override
+    public void setContentView(View view) {
+        if(that != null){
+            that.setContentView(view);
+        } else {
+            super.setContentView(view);
+        }
+    }
+
+    @Override
+    public void setContentView(int layoutResID) {
+        if(that != null){
+            that.setContentView(layoutResID);
+        } else {
+            super.setContentView(layoutResID);
+        }
+    }
+
+    @Override
+    public void startActivity(Intent intent) {
+        Intent m  = new Intent();
+        m.putExtra("className",intent.getComponent().getClassName());
+        that.startActivity(m);
+    }
+
+    @Override
+    public View findViewById(int id) {
+        return that.findViewById(id);
+    }
+
+    @Override
+    public void onCreate(Bundle saveInstanceState) {
+
+    }
+
+    @Override
+    public void onStart() {
+
+    }
+
+    @Override
+    public void onResume() {
+
+    }
+
+    @Override
+    public void onPause() {
+
+    }
+
+    @Override
+    public void onStop() {
+
+    }
+
+    @Override
+    public void onDestroy() {
+
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+
+    }
+}
---
+++ 7.dn架构师/code/lsn23/DN_ALAN_PLUGIN_23/taopiaopiao/src/main/java/com/dn_alan/taopiaopiao/TaoMainActivity.java
@@ -0,0 +1,26 @@
+package com.dn_alan.taopiaopiao;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.dn_alan.pluginstand.PayInterfaceActivity;
+
+public class TaoMainActivity extends BaseActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        findViewById(R.id.img).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(that,"插件",Toast.LENGTH_SHORT).show();
+                startActivity(new Intent(that, SceondActivity.class));
+            }
+        });
+    }
+}
